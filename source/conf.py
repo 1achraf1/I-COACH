776e3b63857087d0428d74322eb070d6cae26881
KASSIMI_Achraf <154908353+1achraf1@users.noreply.github.com>
2025-06-15 21:47:25 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -19,6 +19,7 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = []
 
+html_theme = "sphinx_rtd_theme"
 
 
 # -- Options for HTML output -------------------------------------------------
